1e3786c36d037b61fc4da341b0ff8901e54262dc
pressureless <190361902@qq.com>
2021-07-15 21:13:42 -0400

    Update

--- inference.py
+++ inference.py
@@ -39,8 +39,6 @@ class TypeConstraint(object):
 
 # =======================================================#
 # Class definitions for the abstract syntax tree nodes
-# which comprise the little language for which types
-# will be inferred
 
 class AstTreeNode(object):
     def __init__(self):
@@ -129,7 +127,6 @@ class Letrec(AstTreeNode):
 
 class InferenceError(Exception):
     """Raised if the type inference algorithm cannot infer types successfully"""
-
     def __init__(self, message):
         self.__message = message
 
@@ -141,7 +138,6 @@ class InferenceError(Exception):
 
 class ParseError(Exception):
     """Raised if the type environment supplied for is incomplete"""
-
     def __init__(self, message):
         self.__message = message
 
@@ -156,13 +152,10 @@ class ParseError(Exception):
 
 class TypeVariable(object):
     """A type variable standing for an arbitrary type.
-
     All type variables have a unique id, but names are only assigned lazily,
     when required.
     """
-
     next_variable_id = 0
-
     def __init__(self):
         self.id = TypeVariable.next_variable_id
         TypeVariable.next_variable_id += 1
@@ -201,7 +194,9 @@ class TypeScheme(object):
         new_types = []
         for ty in self.type_op.types:
             if ty in self.quantified_types:
-                new_types.append(TypeVariable())
+                new_type = TypeVariable()
+                print("Instantiate Name:{}".format(new_type))
+                new_types.append(new_type)
             else:
                 new_types.append(ty)
         return TypeOperator(self.name, new_types)
@@ -270,11 +265,13 @@ def instantiate(x):
     else:
         return x
 
+
 def get_active_vars(cons):
     atvs = set()
     for c in cons:
         atvs.union(c.get_active_vars())
     return atvs
+
 
 def ftv(x):
     if isinstance(x, Function):
@@ -293,6 +290,7 @@ def ftv(x):
 
 # Basic types are constructed with a nullary type constructor
 Integer = TypeOperator("int", [])  # Basic integer
+Double = TypeOperator("double", [])  # Basic double
 Bool = TypeOperator("bool", [])  # Basic bool
 
 
@@ -333,30 +331,43 @@ def analyse(node, env=None):
         else:
             v_type = TypeVariable()
             assum[node.name] = v_type
+            print("Identifier Name {}:{}".format(node.name, v_type))
         return v_type, assum, cons
     elif isinstance(node, Apply):
         fun_type, assum, cons1 = analyse(node.fn, env)
         arg_type, assum2, cons2 = analyse(node.arg, env)
         result_type = TypeVariable()
-        assum.update(assum2)
+        print("Apply Name: {}".format(result_type))
         cons = cons1.union(cons2)
+        same_keys = set(assum.keys()).intersection(assum2.keys())
+        if len(same_keys) > 0:
+            for k in same_keys:
+                cons.add(TypeConstraint(assum[k], assum2[k], ConsType.ConsEq))
+        assum.update(assum2)
         cons.add(TypeConstraint(Function(arg_type, result_type), fun_type, ConsType.ConsEq))
         return result_type, assum, cons
     elif isinstance(node, Lambda):
         arg_type = TypeVariable()
         node.body.add_m(arg_type)
         result_type, assum, cons = analyse(node.body, env)
-        x_type = assum[node.v]
-        del assum[node.v]
-        cons.add(TypeConstraint(x_type, arg_type, ConsType.ConsEq))
+        if node.v in assum:
+            # parameter may not be used inside function
+            x_type = assum[node.v]
+            del assum[node.v]
+            cons.add(TypeConstraint(x_type, arg_type, ConsType.ConsEq))
+        print("Lambda Name {}:{}".format(node.v, arg_type))
         return Function(arg_type, result_type), assum, cons
     elif isinstance(node, Let):
         defn_type, assum, cons1 = analyse(node.defn, env)
         body_type, assum2, cons2 = analyse(node.body, env)
         x_type = assum2[node.v]
+        cons = cons1.union(cons2)
+        same_keys = set(assum.keys()).intersection(assum2.keys())
+        if len(same_keys) > 0:
+            for k in same_keys:
+                cons.add(TypeConstraint(assum[k], assum2[k], ConsType.ConsEq))
         assum.update(assum2)
         del assum[node.v]
-        cons = cons1.union(cons2)
         cons.add(TypeConstraint(x_type, defn_type, ConsType.ConsLess, node.m_set))
         return body_type, assum, cons
     elif isinstance(node, Letrec):
@@ -371,86 +382,6 @@ def analyse(node, env=None):
         return analyse(node.body, new_env, non_generic), assum, cons
     assert 0, "Unhandled syntax node {0}".format(type(node))
 
-
-def get_type(name, env, non_generic):
-    """Get the type of identifier name from the type environment env.
-
-    Args:
-        name: The identifier name
-        env: The type environment mapping from identifier names to types
-        non_generic: A set of non-generic TypeVariables
-
-    Raises:
-        ParseError: Raised if name is an undefined symbol in the type
-            environment.
-    """
-    if name in env:
-        return fresh(env[name], non_generic)
-    elif is_integer_literal(name):
-        return Integer
-    else:
-        raise ParseError("Undefined symbol {0}".format(name))
-
-
-def fresh(t, non_generic):
-    """Makes a copy of a type expression.
-
-    The type t is copied. The the generic variables are duplicated and the
-    non_generic variables are shared.
-
-    Args:
-        t: A type to be copied.
-        non_generic: A set of non-generic TypeVariables
-    """
-    mappings = {}  # A mapping of TypeVariables to TypeVariables
-
-    def freshrec(tp):
-        p = prune(tp)
-        if isinstance(p, TypeVariable):
-            if is_generic(p, non_generic):
-                if p not in mappings:
-                    mappings[p] = TypeVariable()
-                return mappings[p]
-            else:
-                return p
-        elif isinstance(p, TypeOperator):
-            return TypeOperator(p.name, [freshrec(x) for x in p.types])
-
-    return freshrec(t)
-
-
-# def unify(t1, t2):
-#     """Unify the two types t1 and t2.
-#
-#     Makes the types t1 and t2 the same.
-#
-#     Args:
-#         t1: The first type to be made equivalent
-#         t2: The second type to be be equivalent
-#
-#     Returns:
-#         None
-#
-#     Raises:
-#         InferenceError: Raised if the types cannot be unified.
-#     """
-#
-#     a = prune(t1)
-#     b = prune(t2)
-#     if isinstance(a, TypeVariable):
-#         if a != b:
-#             if occurs_in_type(a, b):
-#                 raise InferenceError("recursive unification")
-#             a.instance = b
-#     elif isinstance(a, TypeOperator) and isinstance(b, TypeVariable):
-#         unify(b, a)
-#     elif isinstance(a, TypeOperator) and isinstance(b, TypeOperator):
-#         if a.name != b.name or len(a.types) != len(b.types):
-#             raise InferenceError("Type mismatch: {0} != {1}".format(str(a), str(b)))
-#         for p, q in zip(a.types, b.types):
-#             unify(p, q)
-#     else:
-#         assert 0, "Not unified"
 
 ### == Constraint Solver ==
 from collections import deque
@@ -533,7 +464,7 @@ def unify(x, y):
         if len(x.types) != len(y.types):
             return Exception("Wrong number of arguments")
         # s1 = solve(zip([x.types[0]], [y.types[0]]))
-        s1 = solve([TypeConstraint(x.types[0], x.types[1], ConsType.ConsEq)])
+        s1 = solve_cons([TypeConstraint(x.types[0], y.types[0], ConsType.ConsEq)])
         s2 = unify(apply(s1, x.types[1]), apply(s1, y.types[1]))
         return compose(s2, s1)
     elif isinstance(x, TypeVariable):
@@ -542,16 +473,6 @@ def unify(x, y):
         return bind(y.name, x)
     else:
         raise InferError(x, y)
-
-def solve(xs):
-    mgu = empty()
-    cs = deque(xs)
-    while len(cs):
-        cons = cs.pop()
-        s = unify(cons.lhs, cons.rhs)
-        mgu = compose(s, mgu)
-        cs = deque(applyList(s, cs))
-    return mgu
 
 
 def split_cons(cons):
@@ -571,6 +492,7 @@ def split_cons(cons):
         next_con = next_m_cons if next_m_cons is not None else next_le_cons
         cons.remove(next_con)
         return next_con, cons
+
 
 def generalize(env, x):
     if isinstance(x, TypeOperator):
@@ -621,90 +543,8 @@ def compose(s1, s2):
     s3 = dict((t, apply(s1, u)) for t, u in s2.items())
     return union(s1, s3)
 
-def prune(t):
-    """Returns the currently defining instance of t.
-
-    As a side effect, collapses the list of type instances. The function Prune
-    is used whenever a type expression has to be inspected: it will always
-    return a type expression which is either an uninstantiated type variable or
-    a type operator; i.e. it will skip instantiated variables, and will
-    actually prune them from expressions to remove long chains of instantiated
-    variables.
-
-    Args:
-        t: The type to be pruned
-
-    Returns:
-        An uninstantiated TypeVariable or a TypeOperator
-    """
-    if isinstance(t, TypeVariable):
-        if t.instance is not None:
-            t.instance = prune(t.instance)
-            return t.instance
-    return t
-
-
-def is_generic(v, non_generic):
-    """Checks whether a given variable occurs in a list of non-generic variables
-
-    Note that a variables in such a list may be instantiated to a type term,
-    in which case the variables contained in the type term are considered
-    non-generic.
-
-    Note: Must be called with v pre-pruned
-
-    Args:
-        v: The TypeVariable to be tested for genericity
-        non_generic: A set of non-generic TypeVariables
-
-    Returns:
-        True if v is a generic variable, otherwise False
-    """
-    return not occurs_in(v, non_generic)
-
-
-def occurs_in_type(v, type2):
-    """Checks whether a type variable occurs in a type expression.
-
-    Note: Must be called with v pre-pruned
-
-    Args:
-        v:  The TypeVariable to be tested for
-        type2: The type in which to search
-
-    Returns:
-        True if v occurs in type2, otherwise False
-    """
-    pruned_type2 = prune(type2)
-    if pruned_type2 == v:
-        return True
-    elif isinstance(pruned_type2, TypeOperator):
-        return occurs_in(v, pruned_type2.types)
-    return False
-
-
-def occurs_in(t, types):
-    """Checks whether a types variable occurs in any other types.
-
-    Args:
-        t:  The TypeVariable to be tested for
-        types: The sequence of types in which to search
-
-    Returns:
-        True if t occurs in any of types, otherwise False
-    """
-    return any(occurs_in_type(t, t2) for t2 in types)
-
 
 def is_integer_literal(name):
-    """Checks whether name is an integer literal string.
-
-    Args:
-        name: The identifier to check
-
-    Returns:
-        True if name is an integer literal, otherwise False
-    """
     result = True
     try:
         int(name)
@@ -730,24 +570,22 @@ def try_exp(env, node):
     global constraint
     constraint = []
     print(str(node) + " : ", end=' ')
+    print('\n')
     try:
         t, assum, cons = analyse(node, env)
         print("\nassumption: {}".format(assum))
         print("\nTotal cons: {}".format(len(cons)))
-        # print("\nTotal cons: {}".format(cons))
         for item in assum:
             if item not in env:
                 raise Exception("Undefined variables exist")
             cons.add(TypeConstraint(env[assum], env[item], ConsType.ConsEq))
         for con in cons:
             print(con)
-        print("str: {}\n".format(str(t)))
-        mgu = solve(cons)
-        mgu1 = solve_cons(cons)
+        mgu = solve_cons(cons)
         print("mgu: {}\n".format(mgu))
-        print("mgu1: {}\n".format(mgu1))
-        infer_ty = apply(mgu, node)
-        print("infer_ty: {}\n".format(infer_ty))
+        infer_ty = apply(mgu, t)
+        print("infer_ty str  : {}".format(str(t)))
+        print("infer_ty value: {}".format(infer_ty))
         res = retrieve_type(infer_ty)
         print("res: {}\n".format(res))
     except (ParseError, InferenceError) as e:
@@ -777,6 +615,7 @@ def main():
               "cond": Function(Bool, Function(var3, Function(var3, var3))),
               "zero": Function(Integer, Bool),
               "pred": Function(Integer, Integer),
+              "add": Function(Integer, Function(Integer, Integer)),
               "times": Function(Integer, Function(Integer, Integer))}
 
     pair = Apply(Apply(Identifier("pair"),
@@ -787,41 +626,41 @@ def main():
 
     examples = [
         # factorial
-        Letrec("factorial",  # letrec factorial =
-               Lambda("n",  # fn n =>
-                      Apply(
-                          Apply(  # cond (zero n) 1
-                              Apply(Identifier("cond"),  # cond (zero n)
-                                    Apply(Identifier("zero"), Identifier("n"))),
-                              Identifier("1")),
-                          Apply(  # times n
-                              Apply(Identifier("times"), Identifier("n")),
-                              Apply(Identifier("factorial"),
-                                    Apply(Identifier("pred"), Identifier("n")))
-                          )
-                      )
-                      ),  # in
-               Apply(Identifier("factorial"), Identifier("5"))
-               ),
+        # Letrec("factorial",  # letrec factorial =
+        #        Lambda("n",  # fn n =>
+        #               Apply(
+        #                   Apply(  # cond (zero n) 1
+        #                       Apply(Identifier("cond"),  # cond (zero n)
+        #                             Apply(Identifier("zero"), Identifier("n"))),
+        #                       Identifier("1")),
+        #                   Apply(  # times n
+        #                       Apply(Identifier("times"), Identifier("n")),
+        #                       Apply(Identifier("factorial"),
+        #                             Apply(Identifier("pred"), Identifier("n")))
+        #                   )
+        #               )
+        #               ),  # in
+        #        Apply(Identifier("factorial"), Identifier("5"))
+        #        ),
 
         # Should fail:
         # fn x => (pair(x(3) (x(true)))
-        Lambda("x",
-               Apply(
-                   Apply(Identifier("pair"),
-                         Apply(Identifier("x"), Identifier("3"))),
-                   Apply(Identifier("x"), Identifier("true")))),
+        # Lambda("x",
+        #        Apply(
+        #            Apply(Identifier("pair"),
+        #                  Apply(Identifier("x"), Identifier("3"))),
+        #            Apply(Identifier("x"), Identifier("true")))),
 
         # pair(f(3), f(true))
-        Apply(
-            Apply(Identifier("pair"), Apply(Identifier("f"), Identifier("4"))),
-            Apply(Identifier("f"), Identifier("true"))),
+        # Apply(
+        #     Apply(Identifier("pair"), Apply(Identifier("f"), Identifier("4"))),
+        #     Apply(Identifier("f"), Identifier("true"))),
 
         # let f = (fn x => x) in ((pair (f 4)) (f true))
-        Let("f", Lambda("x", Identifier("x")), pair),
+        # Let("f", Lambda("x", Identifier("x")), pair),
 
         # fn f => f f (fail)
-        Lambda("f", Apply(Identifier("f"), Identifier("f"))),
+        # Lambda("f", Apply(Identifier("f"), Identifier("f"))),
 
         # let g = fn f => 5 in g g
         Let("g",
@@ -830,26 +669,27 @@ def main():
 
         # example that demonstrates generic and non-generic variables:
         # fn g => let f = fn x => g in pair (f 3, f true)
-        Lambda("g",
-               Let("f",
-                   Lambda("x", Identifier("g")),
-                   Apply(
-                       Apply(Identifier("pair"),
-                             Apply(Identifier("f"), Identifier("3"))
-                             ),
-                       Apply(Identifier("f"), Identifier("true"))))),
+        # Lambda("g",
+        #        Let("f",
+        #            Lambda("x", Identifier("g")),
+        #            Apply(
+        #                Apply(Identifier("pair"),
+        #                      Apply(Identifier("f"), Identifier("3"))
+        #                      ),
+        #                Apply(Identifier("f"), Identifier("true"))))),
 
         # Function composition
         # fn f (fn g (fn arg (f g arg)))
-        Lambda("f", Lambda("g", Lambda("arg", Apply(Identifier("g"), Apply(Identifier("f"), Identifier("arg"))))))
+        # Lambda("f", Lambda("g", Lambda("arg", Apply(Identifier("g"), Apply(Identifier("f"), Identifier("arg"))))))
     ]
 
     # try_exp(my_env, Apply(Identifier("zero"), Identifier("4")))
     # try_exp(my_env, Let("f", Lambda("x", Identifier("x")), Apply(Identifier("f"), Identifier("4"))))
     # try_exp(my_env, Let("y", Identifier("m"), Let("x", Apply(Identifier("y"), Identifier("4")), Identifier("x"))))
-    try_exp(my_env, Lambda("m", Let("y", Identifier("m"), Let("x", Apply(Identifier("y"), Identifier("4")), Identifier("x")))))
-    # for example in examples:
-    #     try_exp(my_env, example)
+    # try_exp(my_env, Lambda("m", Let("y", Identifier("m"), Let("x", Apply(Identifier("y"), Identifier("4")), Identifier("x")))))
+    # try_exp(my_env, Lambda("x", Lambda("y", Apply(Apply(Identifier("add"), Identifier("x")), Identifier("3")))))
+    for example in examples:
+        try_exp(my_env, example)
 
 
 if __name__ == '__main__':
